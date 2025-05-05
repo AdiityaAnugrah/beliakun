e3f73d216b988a579a1e6aaed2d758f9e2dfd28b
Aditya Anugrah <134223672+AdiityaAnugrah@users.noreply.github.com>
2025-05-05 19:55:46 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,23 @@ import Footer from "./components/Footer";
 
 function App() {
     return (
-        <>
+        <div style={{ minHeight: "100svh" }} className="flex flex-col">
             <Navbar />
-            <div className="flex flex-col" style={{ minHeight: "100svh" }}>
-                <div style={{ flex: 1 }}>
+            <div style={{ flex: 1, position: "relative" }}>
+                <div
+                    style={{
+                        position: "absolute",
+                        top: "0",
+                        left: "0",
+                        width: "100%",
+                        height: "100%",
+                    }}
+                >
                     <Outlet />
                 </div>
-                <Footer />
             </div>
-        </>
+            <Footer />
+        </div>
     );
 }
 
